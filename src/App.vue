38f5fea8d2ee4c04046f68a11ed1f9595c22973e
valeriy <valerih333@gmail.com>
2021-02-15 19:40:35 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/breed-selector">Breed Selector</router-link> |
+    <router-link to="/favorites">Favorites</router-link>
   </div>
   <router-view />
 </template>
---
+++ src/views/Favorites.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    This is the favourites page
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
